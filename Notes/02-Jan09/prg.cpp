0ea7902544fc4edc7ad63194dbd8c3ac728f0f76
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2025-01-09 11:27:37 -0500

    Introducing someone...

--- Notes/02-Jan09/prg.cpp
+++ Notes/02-Jan09/prg.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 int main( ) {
    cout << "Hello OOP244, jan 09 lecture" << endl;
-   return 0;
+   cout << "Hello I am Fred and I am " << 50 << "Years old and I make " << 12345.67 << " dollars a year!" << endl;
+   return 0; 
 }
