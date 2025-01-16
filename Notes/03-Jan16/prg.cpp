583dafd32792378ee2a93bc237068b02c956c1f7
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2025-01-16 12:22:19 -0500

    default value for function arguments.

--- Notes/03-Jan16/prg.cpp
+++ Notes/03-Jan16/prg.cpp
@@ -1,9 +1,14 @@
-#include "bar.h"
-using namespace seneca;
+#include <iostream>
+using namespace std;
+void bar( int len = 79, char fill = '-' );
 int main( ) {
    bar( 20, '=' );
    bar( 40 );
    bar( );
    bar( 'A' );
    return 0;
+}
+void bar( int len, char fill ) {
+   for ( int i = 0; i < len; cout << fill, i++ );
+   cout << endl;
 }
