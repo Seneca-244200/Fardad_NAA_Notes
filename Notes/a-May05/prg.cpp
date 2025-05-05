e9ee5622dc77397e52823c40d9fb1141b116d5b2
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2025-05-05 15:38:01 -0400

    added stuff

--- Notes/a-May05/prg.cpp
+++ Notes/a-May05/prg.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 void hello( ) {
-   cout << "Hello OOP244" << endl;
+   cout << "Hello OOP244 NAA" << endl;
 }
 
 int main( ) {
+   cout << "About to say hello!" << endl;
    hello( );
+   cout << "done saying hello!" << endl;
    return 0;
 }
