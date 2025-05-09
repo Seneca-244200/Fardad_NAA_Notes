5cc7495096479e1d0d1ce1ac20a43e2919344c00
Fardad Soleimanloo <fardad.soleimanloo@senecapolytechnic.ca>
2025-05-09 12:47:02 -0400

    completed getNum() function

--- Notes/b-May06/prg.cpp
+++ Notes/b-May06/prg.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
+int getNum() {
+   int number;
+   cin >> number;
+   return number;
+}
+
+
 int main() {
+   int age;
    cout << "OOP244 NAA May09" << endl;
+   cout << "How old are you?\n> ";
+   age = getNum();
+   cout << "You are " << age << " years old!" << endl;
+
    return 0;
 }
