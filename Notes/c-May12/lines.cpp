637b4258f68625a1a2d772351c2d5e50668c9bab
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2025-05-12 15:48:30 -0400

    Line WITH using namespace std;

--- Notes/c-May12/lines.cpp
+++ Notes/c-May12/lines.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include "lines.h"
+using namespace std;
 namespace seneca {
    void line( size_t len ) {
       for ( size_t i = 0; i < len; i++ ) {
-         std::cout << "-";
+         cout << "-";
       }
-      std::cout << std::endl;
+      cout << endl;
    }
 
    void line( size_t len, char fill ) {
       for ( size_t i = 0; i < len; i++ ) {
-         std::cout << fill;
+         cout << fill;
       }
-      std::cout << std::endl;
+      cout << endl;
    }
 
 
