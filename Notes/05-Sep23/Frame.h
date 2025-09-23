7db7324aaf330a1a0556f73b79c4472af4ec0770
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2025-09-23 16:15:42 -0400

    struct kind of oop

--- Notes/05-Sep23/Frame.h
+++ Notes/05-Sep23/Frame.h
@@ -3,9 +3,11 @@
 #define SENECA_FRAME_H
 namespace seneca {
    struct Frame {
+   private:
       int m_width;
       int m_height;
       char m_fill;
+   public:
       void set( int width, 
          int height, char fill);
       void draw()const;// can't change the owner
