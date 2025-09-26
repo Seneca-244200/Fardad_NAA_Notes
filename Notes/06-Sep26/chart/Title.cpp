0aad627a7e717a901335bbe8cde4d7b114683362
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2025-09-26 12:56:08 -0400

    sctr added and title created

--- Notes/06-Sep26/chart/Title.cpp
+++ Notes/06-Sep26/chart/Title.cpp
@@ -1,6 +1,21 @@
 #include "Title.h"
+#include "cstr.h"
 namespace seneca {
+   Title::Title(const char* content) {
+      if (content && content[0]) {// content exists and has data
+         m_content = new char[strlen(content) + 1];
+         strcpy(m_content, content);
+      }
+      else {
+         m_content = nullptr;
+      }
+   }
+   Title::~Title() {
+      delete[] m_content;
+   }
 
-
+   bool Title::isValid() const {
+      return bool(m_content);
+   }
 
 }
--- Notes/06-Sep26/chart/Title.h
+++ Notes/06-Sep26/chart/Title.h
@@ -3,6 +3,12 @@
 #define SENECA_TITLE_H
 namespace seneca {
    class Title {
+      char* m_content;
+      size_t m_disSize;
+   public:
+      Title(const char* content);
+      ~Title();
+      bool isValid()const;
    };
 }
 #endif // !SENECA_TITLE_H
