808c9f334a72d47f1097e363b1032d64bc850ae1
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2025-10-07 16:55:34 -0400

    oct 07

--- Notes/09-Oct07/Bottle.cpp
+++ Notes/09-Oct07/Bottle.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include "Bottle.h"
 #include "cstr.h"
+using namespace std;
 namespace seneca {
    Bottle::Bottle(const char* content, unsigned int capacity ) {
       if(content && content[0])
@@ -14,6 +16,7 @@ namespace seneca {
       m_curAmount += amount;
       if (m_curAmount > m_capacity) {
          m_curAmount = m_capacity;
+         m_overflow = true;
       }
       return *this;
    }
@@ -21,6 +24,7 @@ namespace seneca {
       m_curAmount = amount;
       if (m_curAmount > m_capacity) {
          m_curAmount = m_capacity;
+         m_overflow = true;
       }
       return *this;
    }
@@ -28,9 +32,55 @@ namespace seneca {
       //return *this = other.m_curAmount;
       return operator=(other.m_curAmount);
    }
-   std::ostream& Bottle::display( std::ostream& ostr ) {
-      return ostr << m_content << " Bottle(" 
-         << m_curAmount << "/" 
+    Bottle Bottle::operator+(const Bottle& other)const {
+      Bottle S("S", this->m_capacity +other.m_capacity);
+      return S = this->m_curAmount + other.m_curAmount;
+   }
+   Bottle& Bottle::operator++() {
+      return operator+=(1);
+   }
+   Bottle Bottle::operator++(int) {
+      Bottle old = *this; // faking post increment
+      operator+=(1);
+      return old;
+   }
+   Bottle::operator int() const {
+      return int(m_curAmount);
+   }
+   Bottle& Bottle::clear() {
+      m_overflow = false;
+
+   }
+   // planning for a detectable safe empty state
+   Bottle::operator bool() const {
+      return !m_overflow;
+   }
+   unsigned int Bottle::operator[](size_t index) const {
+      unsigned int val = -1;
+      if (index == 0) 
+         val = m_capacity;
+      else if (index == 1) 
+         val = m_curAmount;
+      return val;
+   }
+   // bad implementation, needs friendship
+   //int& operator+=(int& left, const Bottle& right) {
+   //   left += right.m_curAmount;
+   //   return left;
+   //}
+   int& operator+=(int& left, const Bottle& right) {
+      left += int(right);
+      return left;
+   }
+   std::ostream& Bottle::display(std::ostream& ostr)const {
+       ostr << m_content << " Bottle("
+         << m_curAmount << "/"
          << m_capacity << ")";
+   }
+
+   ostream& operator<<(ostream& left, const Bottle& right) {
+      if (!right)
+         left << "Spilled ";
+      return right.display(left);
    }
 }
--- Notes/09-Oct07/prg.cpp
+++ Notes/09-Oct07/prg.cpp
@@ -3,18 +3,13 @@
 using namespace std;
 using namespace seneca;
 int main() {
-   Bottle B = "B";
    Bottle C("C", 500);
    C = 300;
-   C.display() << endl;
-   B = 100;
-   cout << "09-Oct07 OOP244 NAA" << endl;
-   B.display( ) << endl;
-   B += 120;
-   B.display() << endl;
-   B += 200;
-   B.display() << endl;
-   C = B;
-   C.display();
+
+   cout << "Here is " << C << endl;
+
+   cout << "Capacity is " << C[0] << endl;
+   cout << "Amount is " << C[1] << endl;
+
    return 0;
 }
